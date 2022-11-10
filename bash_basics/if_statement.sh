8cb00cab0786ea65ba06a5546dd4ae5699d6d7c6
Isabel <iarribas01@gmail.com>
2022-11-10 19:48:46 +0000

    working through lesson 4

---
+++ bash_basics/if_statement.sh
@@ -0,0 +1,21 @@
+var='Im eating pizza'
+
+# if true
+# then
+#   echo $var
+# fi
+
+# if [[ -n $var ]]
+# then
+#   echo $var
+# fi
+
+# if [[ 1 -eq 1 ]]
+# then
+#   echo $var
+#   if [[ -n $var ]]
+#   then
+#     echo $var
+#   fi
+# fi
+
